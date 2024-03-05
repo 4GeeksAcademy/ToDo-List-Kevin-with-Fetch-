0bce01a49ad34300cd8d3eb3938f8d737cf666f9
KevinGaBo <153501399+KevinGaBo@users.noreply.github.com>
2024-03-05 09:52:51 +0000

    last commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,37 +1,55 @@
 import React, { useState } from "react";
 import { FaRegTrashAlt } from "react-icons/fa";
 
-//include images into your bundle
-
-//create your first component
 const Home = () => {
 	const [task, setTask] = useState (["sacar al perro", "hacer la cama","preparar la cena"])
 	const [input, setInput] = useState ("")
-	const [isHovered, setIsHovered] = useState(false)
+	const [isHovered, setIsHovered] = useState([])
 
 	const handleChange = (event) => {setInput(event.target.value)}
-	console.log (input);
+
 	const handleKeyDown = (event) => {
 		if (event.key === "Enter") {
 			setTask ([...task,input])
 			setInput ("")
 		}
 	}
+	const handleMouseEnter = (index) => {
+        const newHovered = [...isHovered];
+		newHovered[index] = true;
+		setIsHovered(newHovered);
+	};
+		
+	const handleMouseLeave = (index) => {
+        const newHovered = [...isHovered];
+		newHovered[index] = false;
+		setIsHovered(newHovered);
+	};
 
+	const handleDeleteTask = (index) => {
+		const newTask = [...task];
+		newTask.splice (index, 1);
+		setTask (newTask);
+	};
 
 	return (
+		<div className="home-container">
 		<div className="taskNews">
-			<input type="text" name="" id="" onChange={handleChange} value={input} onKeyDown={handleKeyDown} />
+			<input type="text" name="" id="" placeholder="¿Que tienes que hacer?" onChange={handleChange} value={input} onKeyDown={handleKeyDown} />
 			<ul>
-				{task.map((tarea) => {
+				{task.map((tarea, index) => {
 					return (
-						<li onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+						<li 
+						key={index}
+                        onMouseEnter={() => handleMouseEnter(index)}
+                        onMouseLeave={() => handleMouseLeave(index)}>
 							{tarea}
-							{isHovered && <FaRegTrashAlt />}
+							{isHovered[index] && <FaRegTrashAlt onClick={() => handleDeleteTask(index)}/>}
 						</li>
-					)
+					);
 				})}
 			</ul>
+		</div>
 		</div>
 	);
 };
